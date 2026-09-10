83ccf1bc29597225998125f8c01a36c2abd0acc0
emianuelul <emanuel.marin0000@gmail.com>
2026-09-10 14:55:27 +0300

    add read write fns for registers + cpsr constants + start condition code impl

--- rust-emulator/rustlib/src/cpu_module.rs
+++ rust-emulator/rustlib/src/cpu_module.rs
@@ -1,12 +1,22 @@
 use crate::memory_area::GBAMemory;
 use std::collections::HashMap;
-use tracing::error;
+use tracing::{error, warn};
 
 // fetch
 //   V
 // decode
 //   V
 // execute
+
+const PC: usize = 15;
+const N_FLAG: usize = 31;
+const Z_FLAG: usize = 30;
+const C_FLAG: usize = 29;
+const V_FLAG: usize = 28;
+
+const IRQ_FLAG: usize = 7;
+const FIQ_FLAG: usize = 6;
+const STATE_FLAG: usize = 5;
 
 #[derive(Hash, Eq, PartialEq)]
 enum CPUMode {
@@ -21,8 +31,27 @@ enum CPUMode {
 }
 
 enum CPUState {
-    ARM,
-    THUMB,
+    Arm,
+    Thumb,
+}
+
+enum ConditionCode {
+    EQ,
+    NE,
+    CSHS,
+    CCLO,
+    MI,
+    PL,
+    VS,
+    VC,
+    HI,
+    LS,
+    GE,
+    LT,
+    GT,
+    LE,
+    AL,
+    NV,
 }
 
 // pointers: r13, r14 (SP, LR)
@@ -87,11 +116,19 @@ impl CPU {
         ((copy >> index) & 1) as u8
     }
 
-    fn get_cpu_state(&self) -> CPUState {
-        if self.get_cpsr_bit(5) == 0 {
-            CPUState::ARM
+    fn set_cpsr_bit(&mut self, index: usize, value: bool) {
+        if value {
+            self.cpsr |= 1 << index
         } else {
-            CPUState::THUMB
+            self.cpsr &= !(1 << index)
+        }
+    }
+
+    fn get_cpu_state(&self) -> CPUState {
+        if self.get_cpsr_bit(STATE_FLAG) == 0 {
+            CPUState::Arm
+        } else {
+            CPUState::Thumb
         }
     }
 
@@ -117,6 +154,67 @@ impl CPU {
         }
     }
 
+    fn get_effective_cpu_mode(&self) -> CPUMode {
+        if [CPUMode::User, CPUMode::Sys].contains(&self.get_cpu_mode()) {
+            CPUMode::UserSys
+        } else {
+            self.get_cpu_mode()
+        }
+    }
+
+    fn set_register_value(&mut self, index: usize, data: u32) {
+        let curr_mode = self.get_effective_cpu_mode();
+        match index {
+            0..8 => self.registers.common[index] = data,
+            8..=12 => {
+                if curr_mode == CPUMode::Fiq {
+                    self.registers.fiq[index - 8] = data;
+                } else {
+                    self.registers.common[index] = data;
+                }
+            }
+            13..=14 => {
+                let value = self.registers.pointers.get(&curr_mode).unwrap();
+
+                if index == 13 {
+                    self.registers.pointers.insert(curr_mode, (data, value.1));
+                } else {
+                    self.registers.pointers.insert(curr_mode, (value.0, data));
+                }
+            }
+            PC => self.registers.pc = data,
+
+            _ => {
+                error!("CPU Register read index out of bounds: {}", index);
+            }
+        }
+    }
+
+    fn get_register_value(&self, index: usize) -> u32 {
+        let curr_mode = self.get_effective_cpu_mode();
+        match index {
+            0..8 => self.registers.common[index],
+            8..=12 => {
+                if curr_mode == CPUMode::Fiq {
+                    self.registers.fiq[index - 8]
+                } else {
+                    self.registers.common[index]
+                }
+            }
+            13..=14 => {
+                let value = self.registers.pointers.get(&curr_mode).unwrap();
+
+                if index == 13 { value.0 } else { value.1 }
+            }
+            PC => self.registers.pc,
+
+            _ => {
+                error!("CPU Register read index out of bounds: {}", index);
+                0
+            }
+        }
+    }
+
     pub fn step(&mut self, memory: &GBAMemory) -> u32 {
         if self.registers.pc >= memory.get_rom_size() as u32 {
             error!(
@@ -130,8 +228,8 @@ impl CPU {
 
         // fetch
         match self.get_cpu_state() {
-            CPUState::ARM => {}
-            CPUState::THUMB => {}
+            CPUState::Arm => {}
+            CPUState::Thumb => {}
         }
 
         // decode
